55eec6da16251f4540a41de64c74f16e531fde65
MRamirez166 <MRamirezzz133@gmail.com>
2025-03-28 14:22:46 -0400

    3/28/2025
    Today I made the methods and called them.

--- main/src/Main.java
+++ main/src/Main.java
@@ -2,68 +2,96 @@
 Martin Ramirez
 March 27 2025
 */
-import java.util.Random;
-import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
-    }
-    //super class character containing attributes for level and name.
-    class Character{
-        protected int level;
-        protected String name;
-        protected int hit_points;
-        protected int str;
-        protected int dex;
-        protected int con;
-        protected int intelligence;
-        protected int wis;
-        protected int rizz;
-        protected int armor;
-        protected ArrayList<String> labels = new ArrayList<String>();
-        labels.add(0, "label 1");
-        labels.add(1, "label 2");
+        Character c1 = new Character();
+        Barbarian b1 = new Barbarian(1, "Jordan");
 
-        public Character(int level, String name, int hit_points, int str, int dex, int con, int intelligence, int wis, int rizz, int armor){
-            this.level = level;
-            this.name = name;
-            this.hit_points = hit_points;
-            this.str = str;
-            this.dex = dex;
-            this.con = con;
-            this.intelligence = intelligence;
-            this.wis = wis;
-            this.rizz = rizz;
-            this.armor = armor;
-
-        }
-        public Character(){
-            this.level = 1;
-            this.name = "Human";
-            this.hit_points = 3;
-            this.str = 2;
-            this.dex = 1;
-            this.con = 5;
-            this.intelligence = 6;
-            this.wis = 7;
-            this.rizz = 8;
-            this.armor = 9;
-        }
-        //
-        public void roll(){
-            Random rand = new Random();
-        }
-    }
-    class Barbarian extends Character{
-        private int rage;
-
-        public Barbarian(){
-            super(level, name, hit_points, str, dex, con, intelligence, wis, rizz, armor);
-            this.rage = rage;
-        }
-
+        //System.out.print(c1);
+        System.out.print(b1);
     }
 
+//super class character containing attributes for level and name.
+}
+class Character{
+    protected int level;
+    protected String name;
+    protected int hit_points;
+    protected int str;
+    protected int dex;
+    protected int con;
+    protected int intelligence;
+    protected int wis;
+    protected int rizz;
+    protected int armor;
+    protected ArrayList<String> labels;
+
+    public Character(int level, String name){
+        this.level = level;
+        this.name = name;
+        this.hit_points = roll();
+        this.str = roll();
+        this.dex = roll();
+        this.con = roll();
+        this.intelligence = roll();
+        this.wis = roll();
+        this.rizz = roll();
+        this.armor = roll();
+        this.labels = new ArrayList<String>();
+    }
+    public Character(){
+        this.level = 1;
+        this.name = "Human";
+        this.hit_points = 3;
+        this.str = 2;
+        this.dex = 1;
+        this.con = 5;
+        this.intelligence = 6;
+        this.wis = 7;
+        this.rizz = 8;
+        this.armor = 9;
+        this.labels = new ArrayList<String>();
+    }
+
+    //Roll a random number 1-13.
+    public int roll(){
+        int randomNumber = (int)(Math.random()* 14) + 3;
+        System.out.print(randomNumber);
+        return 1;
+    }
+
+    //adds labels to characters.
+    public void features(String feat){
+        labels.add(feat);
+    }
+
+    public String toString() {
+        return "Character:{" +
+                "level:" + level +
+                ", | name:'" + name + '\'' +
+                ", HP:" + hit_points +
+                ", AC:" + armor +
+                ", | STR:" + str +
+                ", | DEX:" + dex +
+                ", | CON:" + con +
+                ", | INT: " + intelligence +
+                ", | WIS: " + wis +
+                ", | CHA:" + rizz +
+                ", | FEATS:" + labels +
+                '}';
+    }
+}
+
+//Another character, Barbarian with additional attributes.
+class Barbarian extends Character{
+
+    public Barbarian(int level, String name){
+        super(level, name);
+        str += 2;
+        con += 1;
+        features("Rage");
+        features("Unarmored Defense");
+    }
 }
